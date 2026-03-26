ec01ea13b9b3a3e7097cd30cc94b8fec6de2fe2e
asevilla16 <angelsevilla1997@gmail.com>
2026-03-25 20:06:39 -0600

    refactor: bug fix from clients screen on mobile

--- src/app/modules/clients/clients.component.ts
+++ src/app/modules/clients/clients.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-declare function customInitFunctions(): void;
-
 @Component({
   selector: 'app-clients',
   standalone: false,
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css',
 })
-export class ClientsComponent implements OnInit {
-  ngOnInit() {
-    customInitFunctions();
-  }
-}
+export class ClientsComponent {}
